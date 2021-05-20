8b5be93b625a8b0490c216e6a6a4d1e36f139c2b
rasheed <hazelrasheed@gmail.com>
2021-05-21 01:21:19 +0300

    Contact Example Done

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,6 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 part 'main.g.dart';
 
-// void main() {
-//   Hive.initFlutter();
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 const String contactsBoxName = "contacts";
 
 @HiveType(typeId: 0)
